d63cf9993c51e8c6270372fa07dc976a3138c99f
Sofiyan21 <86190339+Sofiyan21@users.noreply.github.com>
2024-04-27 10:02:43 +0530

    Add Structure and Index files

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+function validCred() {
+
+    let valid = true;
+    if (valid) {
+
+        open("./pages/index-home.html");
+        
+    } else {
+        alert("Incorrect Credentials")
+    }
+    
+}
